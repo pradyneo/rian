3eecd97305966da4129d57dfb28a504a6af7c1c9
Prady <pradyneo@users.noreply.github.com>
2021-01-01 11:24:18 -0500

    Added background; updated font

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Component } from 'react';
-import { Col, Container, Image, Jumbotron, Row } from 'react-bootstrap';
+import { Card, Col, Container, Image, Jumbotron, Row } from 'react-bootstrap';
 import Header from './Header.jsx';
 
 class App extends Component {
     render() {
         return (
-            <Header/>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col md="auto">
+                        <Card style={{ width: '50rem' }} className="mx-auto my-5 text-center">
+                            <Card.Header style={{ fontSize: '60px' }}>Rian Neog</Card.Header>
+                            <Card.Body>
+                                <Card.Text style={{ fontSize: '30px' }}>Our little bundle of joy!!</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles.css'
+import './styles/styles.css'
 import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('container'))
